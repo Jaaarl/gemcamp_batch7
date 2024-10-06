119d224d79d3ca40fe994c5d592265793aaaff39
jaaarl <jarl13.work@gmail.com>
2024-10-07 01:18:05 +0800

    add: routes for comments

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   root "welcome#index"
   get 'about_us', to: 'welcome#about_us'
   get 'contact', to: 'welcome#contact'
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 end
